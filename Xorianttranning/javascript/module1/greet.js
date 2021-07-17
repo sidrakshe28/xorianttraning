39efee01eb46bfa5a90e1808bede66fae5e77c3d
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-07-17 22:04:25 +0530

    Module1

--- Xorianttranning/javascript/module1/greet.js
+++
@@ -1,7 +0,0 @@
-
-var p = document.getElementById('your_paragraph');
-var btn = document.getElementById('btn');
-var txt = document.getElementById('theText');
-btn.onclick = function(){
-    p.textContent = txt.value;
-};
--- Xorianttranning/javascript/module11/index.js
+++
@@ -1,6 +0,0 @@
-$("#choice").change(function () {
-    if($(this).val() == "0") $(this).addClass("empty");
-    else $(this).removeClass("empty")
-});
-
-$("#choice").change();
